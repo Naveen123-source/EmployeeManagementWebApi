00716f1c7d6fdec574f89dd3985f3e8d00abde68
Naveen <naveennaveen11060@gmail.com>
2025-09-10 20:47:07 +0530

    Updated Program.cs

--- backendWebAPI/Program.cs
+++ backendWebAPI/Program.cs
@@ -19,7 +19,11 @@ builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("EmployeeManagement.Infrastructure") 
+    )
+);
 
 
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
